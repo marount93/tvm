813ff2be7db5042b62c2318f836808ff44b63d41
Maroun Tork <maroun.t93@gmail.com>
2019-01-21 09:46:21 +0200

    print gpu-kernel Grid Dim

--- src/runtime/cuda/cuda_module.cc
+++ src/runtime/cuda/cuda_module.cc
@@ -167,7 +167,14 @@ class CUDAWrappedFunc {
     }
     CUstream strm = static_cast<CUstream>(CUDAThreadEntry::ThreadLocal()->stream);
     ThreadWorkLoad wl = thread_axis_cfg_.Extract(args);
-    CUresult result = cuLaunchKernel(
+/*  std::cout << "Maroun *** CUDALaunch: \n"
+ *           << func_name_
+ *                    << " grid=(" << wl.grid_dim(0) << ","
+ *                             << wl.grid_dim(1) << "," << wl.grid_dim(2) << "), "
+ *                                      << " block=(" << wl.block_dim(0) << ","
+ *                                               << wl.block_dim(1) << "," << wl.block_dim(2) << ")\n";*/
+
+	CUresult result = cuLaunchKernel(
         fcache_[device_id],
         wl.grid_dim(0),
         wl.grid_dim(1),
